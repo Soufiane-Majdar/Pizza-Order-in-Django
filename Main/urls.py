ba955e47b1e45392db5794eea0f04a6b0ffec619
Sσϝιαɳ <soufianemjr3145@gmail.com>
2022-07-21 00:22:24 +0100

    Connect Urls with Views

--- Main/urls.py
+++ Main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('item/<int:id>/', views.item_detail, name='item_detail'),
+    path('contact/', views.contact, name='contact'),
     
 ]
--- User/urls.py
+++ User/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
