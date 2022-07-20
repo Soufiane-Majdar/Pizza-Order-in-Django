1caab88fcc05bf92830c369b6a2c4998fca05bce
Sσϝιαɳ <soufianemjr3145@gmail.com>
2022-07-21 00:10:15 +0100

    Add Urls paths

--- Pizza_Order/urls.py
+++ Pizza_Order/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    # path('jet/', include('jet.urls', 'jet')),
-    # path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  
     path('admin/', admin.site.urls),
+    path('',include('Main.urls')),
+    path('user/',include('User.urls')),
 ]
