c4134b803e3f0eb3cc826a8c120142dacbca1999
Sσϝιαɳ <soufianemjr3145@gmail.com>
2022-07-21 00:16:30 +0100

    Create a test functions in Views

--- Main/views.py
+++ Main/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+#Home function
+def home(request):
+    return HttpResponse("Home")
+
+# item detail function
+def item_detail(request,id):
+    return HttpResponse("Item Detail: " + str(id))
+
+# Contact us function
+def contact(request):
+    return HttpResponse("Contact us")
--- User/views.py
+++ User/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+# Login function
+def login(request):
+    return HttpResponse("Login")
+
+# Register function
+def register(request):
+    return HttpResponse("Register")
+
+# logout function
+def logout(request):
+    return HttpResponse("Logout")
+
+# profile function
+def profile(request):
+    return HttpResponse("Profile")
