7ca755646f26734d2eead7b9dea68743a43a219b
Sσϝιαɳ <soufianemjr3145@gmail.com>
2022-07-22 11:21:28 +0100

    show items from DB, and connect contact us with DB

--- Main/views.py
+++ Main/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
+
+from .models import MenuItem
+from User.models import Contact
+
 # Create your views here.
 
 #Home function
 def home(request):
-    return render(request,'Main/home.html')
+    MenuItems = MenuItem.objects.all()
+    #print(MenuItems)
+    return render(request,'Main/home.html',{'MenuItems':MenuItems})
 
 # item detail function
 def item_detail(request,id):
@@ -12,4 +18,16 @@ def item_detail(request,id):
 
 # Contact us function
 def contact(request):
-    return render(request,'Main/contact.html')
+    if request.method == 'POST':
+        try:
+            name = request.POST['name']
+            email = request.POST['email']
+            subject = request.POST['subject']
+            message = request.POST['message']
+            contact = Contact(name=name,email=email,subject=subject,message=message)
+            contact.save()
+            return render(request,'Main/contact.html',{'success':'Message sent successfully'})
+        except:
+            return render(request,'Main/contact.html',{'error':'Message not sent'})
+    else:
+        return render(request,'Main/contact.html')
